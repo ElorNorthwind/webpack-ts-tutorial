4263873d2dff80887a4cbcaa7763de0692824b38
Lorans <elornorthwind@gmail.com>
2023-01-29 19:27:13 +0300

    4th lesson

---
+++ src/components/Counter.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./Counter.scss";
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+  const increment = (num: number) => {
+    setCount(count + num);
+  };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => increment(1)}>+</button>
+      <button onClick={() => increment(-1)}>-</button>
+    </div>
+  );
+}
