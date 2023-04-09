0a6f3e38bf6b2d718ee06572f2d3dd3a59fa69d1
Lorans <elornorthwind@gmail.com>
2023-04-09 20:37:28 +0300

    some loki fixes (i hate loki so much)

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.stories.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.stories.tsx
@@ -2,14 +2,9 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { Theme } from "app/providers/ThemeProvider";
 import { Article } from "entities/Article";
 import { ArticleBlockType, ArticleType } from "entities/Article/model/types/article";
+import { StoreDecorator } from "shared/config/storybook/StoreDecorator/StoreDecorator";
 import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import ArticleDetailsPage from "./ArticleDetailsPage";
-
-const ArticleDetailsPageStory: ComponentMeta<typeof ArticleDetailsPage> = {
-  title: "pages/ArticleDetailsPage",
-  component: ArticleDetailsPage,
-};
-export default ArticleDetailsPageStory;
 
 const article: Article = {
   id: "1",
@@ -66,6 +61,15 @@ const article: Article = {
     },
   ],
 };
+
+const ArticleDetailsPageStory: ComponentMeta<typeof ArticleDetailsPage> = {
+  title: "pages/ArticleDetailsPage",
+  component: ArticleDetailsPage,
+  decorators: [
+    StoreDecorator({ articleDetails: { data: article, isLoading: false, error: undefined } }),
+  ],
+};
+export default ArticleDetailsPageStory;
 
 const Template: ComponentStory<typeof ArticleDetailsPage> = (args) => (
   <ArticleDetailsPage {...args} />
