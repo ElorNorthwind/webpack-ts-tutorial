692ca531a32865abf04ad7c152ff792eb7b6aa56
Lorans <elornorthwind@gmail.com>
2023-08-02 20:09:36 +0300

    lesson 77: circular dependancy plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,6 +5,7 @@ import webpack from "webpack";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import { BuildOptions } from "./types/config";
 import CopyPlugin from "copy-webpack-plugin";
+import CircularDependencyPlugin from "circular-dependency-plugin";
 
 export function buildPlugins({
   isDev,
@@ -28,6 +29,10 @@ export function buildPlugins({
     }),
     new CopyPlugin({
       patterns: [{ from: paths.locales, to: paths.buildLocales }],
+    }),
+    new CircularDependencyPlugin({
+      exclude: /node_modules/,
+      failOnError: true,
     }),
   ];
   if (isDev) {
--- src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.tsx
+++ src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.tsx
@@ -3,8 +3,8 @@ import { useTranslation } from "react-i18next";
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./ArticleSortSelector.module.scss";
 import { Select, SelectOptions } from "shared/ui/Select/Select";
-import { ArticleSortField } from "../../model/const/articleConsts";
 import { SortOrder } from "shared/types";
+import { ArticleSortField } from "../../model/const/articleConsts";
 
 interface ArticleSortSelectorProps {
   className?: string;
